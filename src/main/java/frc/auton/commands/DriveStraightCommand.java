09596a4e5c1a8a67c9e8ef1b28454467684e5fdb
luketheduke112 <you@example.com>
2020-01-07 19:55:14 -0500

    Added auton and commands folder

---
+++ src/main/java/frc/auton/commands/DriveStraightCommand.java
@@ -0,0 +1,13 @@
+
+//not working yet. Meeting ended before i finished.
+
+package frc.auton.commands;
+
+import org.usfirst.frc.team1038.robot.I2CGyro;
+import org.usfirst.frc.team1038.robot.Robot;
+import org.usfirst.frc.team1038.subsystem.DriveTrain;
+
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.PIDCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
