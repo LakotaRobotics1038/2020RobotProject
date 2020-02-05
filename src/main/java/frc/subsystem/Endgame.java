d0a460f82472deef4eb594bd6529b1a0049c07f3
Ryan Sippy <rdsippy@gmail.com>
2020-02-05 18:00:43 -0500

    Commited Endgame Code

--- src/main/java/frc/subsystem/Endgame.java
+++ src/main/java/frc/subsystem/Endgame.java
@@ -6,12 +6,24 @@ import frc.robot.CANSpark1038;
 import frc.robot.PiReader;
 
 public class Endgame implements Subsystem {
-    private final int endgameMotorPort = 58;
-    private CANSpark1038 endgameMotor = new CANSpark1038(endgameMotorPort, MotorType.kBrushless);
+
+    //Variables
     private static Endgame endgame;
     private boolean isExtending = false;
     private boolean isAdjusting = false;
     private boolean isRetracting = false;
+
+    //Motor
+    private final double ENDGAME_LEFT_MOTOR_SPEED = 0.5;
+    private final double ENDGAME_RIGHT_MOTOR_SPEED = -0.5;
+    private final int ENDGAME_MOTOR_PORT = 58;
+    private CANSpark1038 endgameMotor = new CANSpark1038(ENDGAME_MOTOR_PORT, MotorType.kBrushless);
+
+    /**
+     * Returns the endgame instance created when the robot starrs
+     * 
+     * @return Endgame instance
+     */
 
     public static Endgame getInstance() {
         if (endgame == null) {
@@ -21,9 +33,21 @@ public class Endgame implements Subsystem {
         return endgame;
     }
 
+    /**
+     * Instantiates endgame object
+     */
+
     private Endgame() {
 
     }
+
+    /**
+     * If isExtending is true, Extend the arms
+     * 
+     * If isRetracting is true, Retract the arms
+     * 
+     * If isAdjusting is true, Move the robot to balance out the bar
+     */
 
     public void endgamePeriodic() {
         if (isExtending) {
@@ -37,27 +61,41 @@ public class Endgame implements Subsystem {
         else if (isAdjusting) {
             // TODO Adjust the Robot Position
 
-            if (PiReader.getLeftEndgameSwitchVal()) {
-                endgameMotor.set(.5);
+            if (PiReader.getLeftEndgameSwitchVal() == 1) {
+                endgameMotor.set(ENDGAME_LEFT_MOTOR_SPEED);
             }
 
-            else if (PiReader.getRightEndgameSwitchVal()) {
-                endgameMotor.set(-.5);
+            else if (PiReader.getRightEndgameSwitchVal() == 1) {
+                endgameMotor.set(ENDGAME_RIGHT_MOTOR_SPEED);
             }
         }
     }
+
+    /**
+     * Set the robot to extend the arms
+     */
 
     public void setIsExtending() {
             isExtending = true;
         
     }
 
+    /**
+     * Set the robot to retract the arms
+     */
+    
+    public void setIsRetracting() {
+        isRetracting = true;
+    }
+
+    /**
+     * Set the robot to adjust it's position on the bar
+     */
+
     public void setIsAdjusting() {
         isAdjusting = true;
 
     }
 
-    public void setIsRetracting() {
-        isRetracting = true;
-    }
+
 }
