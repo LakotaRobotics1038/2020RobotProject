9b1fcfa8bb2f0e6af26653370ccf26ec599a104d
Ryan Sippy <rdsippy@gmail.com>
2020-02-05 18:07:39 -0500

    Commited the CANSpark1038 Class

--- src/main/java/frc/robot/CANSpark1038.java
+++ src/main/java/frc/robot/CANSpark1038.java
@@ -3,6 +3,13 @@ package frc.robot;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 
+/**
+ * Creates a new CAN controlled spark max motor controller object
+ * 
+ * @param id   CAN bus ID of motor
+ * @param type Motor type (Brushed or Brushless)
+ */
+
 public class CANSpark1038 extends CANSparkMax {
     public CANSpark1038(int id, CANSparkMaxLowLevel.MotorType type) {
         super(id, type);
